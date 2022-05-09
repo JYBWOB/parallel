cefc04a5677ac509abcb2aea04cce0b3862b4b44
JYBWOB <215370196@qq.com>
2022-05-09 14:06:29 +0800

    openmp

--- OPENMP/run.sh
+++ OPENMP/run.sh
@@ -1,9 +1,9 @@
 # run.sh
 # !/bin/sh
-# PBS -N test
-# PBS -l nodes=4
+# PBS -N run
+# PBS -l nodes=1
 pssh -h $PBS_NODEFILE mkdir -p /home/s2120210448
-pscp -h $PBS_NODEFILE /home/s2120210448/openmp /home/s2120210448
+pscp -h $PBS_NODEFILE /home/s2120210448/code/OPENMP/openmp /home/s2120210448
 #mpiexec -np 4 -machinefile $PBS_NODEFILE /home/s2120210448/openmp
 /home/s2120210448/code/OPENMP/openmp
 
